dfdf3f87d66b37c30e0cbd6e83ef2f8cf887da45
mtoor286 <mtoor@appdev.yearup.org>
2023-04-17 11:13:46 -0400

    first commit

---
+++ src/main/java/com/mt/WorkingWithStrings.java
@@ -0,0 +1,9 @@
+package com.mt;
+
+public class WorkingWithStrings {
+
+    public static void main (String[] args) {
+
+        System.out.println("My superpower is: Mind Manipulation.");
+    }
+}
